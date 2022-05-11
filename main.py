ea75b9078d60942a181f50f72ea91c310fd3defd
Egor <bamditse@gmail.com>
2022-05-11 21:21:03 +0700

    Added error handling

--- main.py
+++ main.py
@@ -5,11 +5,14 @@ from module import Container
 
 def main():
     if len(sys.argv) != 3:
-        print("Incorrect command line!\n"
-              "Waited: command in_file out_file")
+        print('Incorrect command line input')
         sys.exit(1)
 
-    input_file = open(sys.argv[1], "r")
+    try:
+        input_file = open(sys.argv[1], "r")
+    except OSError:
+        print('Opening file error')
+        sys.exit(1)
 
     print('Start')
 
@@ -19,7 +22,14 @@ def main():
     print('Filled container')
 
     # container.sort()
-    output_file = open(sys.argv[2], "w")
+    try:
+        output_file = open(sys.argv[2], "w")
+    except OSError:
+        print('Opening file error')
+        sys.exit(1)
+    finally:
+        input_file.close()
+
     container.write_to(output_file)
     # container.write_two_dim_array_to(output_file)
 
